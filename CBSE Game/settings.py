5d4af097dbacac504f71deb558151e4c3f1ce62d
karanbihani <84910173+karanbihani@users.noreply.github.com>
2021-09-20 17:50:36 +0530

    NEW JUMPING MECHANIC SORTA FIXED

---
+++ CBSE Game/settings.py
@@ -0,0 +1,17 @@
+#This contains all variable options/dependencies (Excpet levels)
+
+from tiles import Tile
+
+
+SCREEN_WIDTH = 1200
+SCREEN_HEIGHT = 700
+
+TILE_SIZE = 64
+
+FPS = 60
+
+PLAYER_SPEED = 8
+GRAVITY = 1
+JUMP_SPEED = -20
+
+JUMP_ACTIVE = True
