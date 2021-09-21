08a345bda3195ef39b3695c454830691971b1b64
karanbihani <84910173+karanbihani@users.noreply.github.com>
2021-09-21 14:02:36 +0530

    Added Coins
    Still working on main menue
    Also when u run it in the bg it iwll print all the coins u have

---
+++ CBSE Game/level_menue.py
@@ -0,0 +1,9 @@
+from level_menue_data import menue_level
+
+class Menue():
+    def __init__(self,menue_level = menue_level) :
+        self.pos = menue_level[0]
+        self.content = menue_level[1]
+        self.max_level = menue_level[2]
+
+    
