23d5d8418ac5155dc0a2600c0058076bd3914f00
karanbihani <84910173+karanbihani@users.noreply.github.com>
2021-09-22 07:25:50 +0530

    Delete CBSE Game directory

--- CBSE Game/level_menue.py
+++
@@ -1,9 +0,0 @@
-from level_menue_data import menue_level
-
-class Menue():
-    def __init__(self,menue_level = menue_level) :
-        self.pos = menue_level[0]
-        self.content = menue_level[1]
-        self.max_level = menue_level[2]
-
-    
